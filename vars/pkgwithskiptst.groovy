6cc38955a5d9628f0c593ee0f48948c985eae3b7
vij_kes <97831607+vijkes@users.noreply.github.com>
2024-09-10 17:27:08 +0530

    Update pkgwithskiptst.groovy

--- vars/pkgwithskiptst.groovy
+++ vars/pkgwithskiptst.groovy
@@ -1,7 +1,7 @@
 def call() {   
      // Define mvn cmd to be executed
-     def mavenCommand = 'mvn package -DskipTests=true'      
-    // Execute mvn cmd in sh
-     sh script: mavenCommand, returnStatus: true
-
+    //  def mavenCommand = 'mvn package -DskipTests=true'      
+    // // Execute mvn cmd in sh
+    //  sh script: mavenCommand, returnStatus: true
+sh 'mvn package -DskipTests=true'  
 }
